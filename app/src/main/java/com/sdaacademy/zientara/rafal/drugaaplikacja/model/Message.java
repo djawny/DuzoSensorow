c2d1861f3f34e7ba682446c2cb911e8f4ff454df
djawny <djawny@op.pl>
2017-04-27 14:31:20 +0200

    new gradle version

--- app/src/main/java/com/sdaacademy/zientara/rafal/drugaaplikacja/model/Message.java
+++ app/src/main/java/com/sdaacademy/zientara/rafal/drugaaplikacja/model/Message.java
@@ -1,9 +1,5 @@
 package com.sdaacademy.zientara.rafal.drugaaplikacja.model;
 
-/**
- * Created by Evil on 04.02.2017.
- */
-
 public class Message {
 
     private static final String ID = "id";
@@ -11,10 +7,8 @@ public class Message {
     private static final String MESSAGE = "message";
     private static final String DESC = "desc";
 
-
     private long id;
     private String title;
     private String message;
     private String desc;
-
 }
